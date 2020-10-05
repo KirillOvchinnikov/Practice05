41f28c96cfd7433b059db494fd2b99daa5f511ea
KirillOvchinnikov <kirill.ovchinnikov.1968@mail.ru>
2020-10-05 20:47:05 +0300

    commit

--- src/Practice05/_9/Recursion5.java
+++ src/Practice05/_9/Recursion5.java
@@ -3,13 +3,14 @@ package ru.mirea.Practice05._9;
 import java.util.Arrays;
 
 public class Recursion5 {
-    private int count = 0, ind = 0;
+    private int count = 0, ind = 0, cnt = 0;
     private int arr[], arr2[];
     int tmp;
 
     public int SetRec(int a, int b)
     {
         arr = new int[a+b];
+        arr2 = new int[a+b];
         for (int i = 0; i < a; i++)
         {
             arr[i] = 0;
@@ -19,7 +20,7 @@ public class Recursion5 {
         for (int i = a+1; i < b; i++)
         {
             arr[i] = 1;
-            arr2[i] = 0;
+            arr2[i] = 1;
         }
 
         if (a < 0 || b < 0)
@@ -40,23 +41,28 @@ public class Recursion5 {
         tmp = arr[ind];
         arr[ind] = arr[ind+1];
         arr[ind+1] = tmp;
-        ind++;
-
-        if (Arrays.equals(arr, arr2))
+        if(ind != a+b-2)
         {
-            return GetAnswer(count);
+            ind++;
         }
 
-        for(int i = 0; i < a; i++)
+
+        for (int i = 0; i < arr.length; i++)
         {
-            for (int j = i+1; i < b; j++)
-            {
-                if (arr[i] == arr[j] && arr[i] == 0)
-                    return rec(a, b);
-                else
-                {
-                    count++;
-                }
+            if (arr[i] == arr2[i]);
+            cnt++;
+        }
+
+        if (cnt == arr.length)
+            return GetAnswer(count);
+
+
+        for(int i = 0; i < a+b-1; i++)
+        {
+            if (arr[i] == arr[i+1] && arr[i] == 0)
+                return rec(a, b);
+            else {
+                count++;
             }
         }
         return rec(a,b);
